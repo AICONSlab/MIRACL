a32571d58ddfa3c24918fb34b202cefed40ebbb5
mgoubran <magedgoubran@gmail.com>
2017-06-17 15:09:03 -0700

    upd extract lbl w down

--- utils/miracl_create_brainmask.py
+++ utils/miracl_create_brainmask.py
@@ -16,7 +16,7 @@ def helpmsg():
 
     Creates brain mask (nii/nii.gz) for CLARITY data
 
-    Usage: miracl_create_brainmask.py -i [input volume] -o [output brain mask] -d [down-sample ratio]
+    Usage: miracl_create_brainmask.py -i [input volume] -o [output brain mask]
 
 Example: miracl_create_brainmask.py -i clarity_downsample_05x_virus_chan.nii.gz -o clarity_brain_mask.nii.gz -d 5
 
@@ -27,8 +27,6 @@ Example: miracl_create_brainmask.py -i clarity_downsample_05x_virus_chan.nii.gz 
     Optional arguments:
 
         -l Output brain mask
-
-        -d Down-sample ratio
 
         '''
 
@@ -43,7 +41,6 @@ def parseinputs():
 
     parser.add_argument('-i', '--invol', type=str, help="In volume", required=True)
     parser.add_argument('-o', '--outfile', type=str, help="Output file")
-    parser.add_argument('-d', '--down', type=int, help="Down-sample ratio")
 
     args = parser.parse_args()
 
--- utils/miracl_extract_lbl.py
+++ utils/miracl_extract_lbl.py
@@ -10,6 +10,7 @@ import sys
 import nibabel as nib
 import numpy as np
 import pandas as pd
+import scipy.ndimage
 
 
 def helpmsg():
@@ -17,15 +18,19 @@ def helpmsg():
 
     Outputs nifti file with only chosen label
 
-    Usage: miracl_extract_lbl.py -i [input (registered) labels] -l [output label]
+    Usage: miracl_extract_lbl.py -i [input (registered) labels] -l [output label] -d [down-sample ratio]
 
-Example: miracl_extract_lbl.py -i clarity_registered_allen_labels.nii.gz -l PL
+Example: miracl_extract_lbl.py -i clarity_registered_allen_labels.nii.gz -l PL -d 5
 
     Arguments (required):
 
         -i Input (registered) Allen labels 
 
         -l Output label (or seed mask) using Allen atlas ontology acronyms
+
+    Optional arguments:
+
+        -d Down-sample ratio
 
         '''
 
@@ -38,6 +43,7 @@ def parseinputs():
 
     parser.add_argument('-i', '--inlbls', type=str, help="In lbls", required=True)
     parser.add_argument('-l', '--outlbl', type=str, help="Out lbl", required=True)
+    parser.add_argument('-d', '--down', type=int, help="Down-sample ratio")
 
     args = parser.parse_args()
 
@@ -52,14 +58,21 @@ def parseinputs():
     assert isinstance(args.lbls, str)
     outlbl = args.outlbl
 
-    return inlbls, outlbl
+    if args.down is None:
+        d = 1
+        print("\n down-sample ratio not specified ... preserving resolution" % d)
+    else:
+        assert isinstance(args.down, int)
+        d = args.down
+
+    return inlbls, outlbl, d
 
 
 # ---------
 
 def main():
     # parse in args
-    [inlbls, outlbl] = parseinputs()
+    [inlbls, outlbl, d] = parseinputs()
 
     # extract lbl
     print("\n Reading input labels ...\n")
@@ -81,10 +94,19 @@ def main():
     # assuming iso resolution
     vx = inlblsnii.header.get_zooms()[0]
 
-    mat = np.eye(4) * vx
+    outvox = vx * d
+
+    mat = np.eye(4) * outvox
     mat[3, 3] = 1
 
     outnii = "%s_mask.nii.gz" % outlbl
+
+    if d > 1:
+        print("\n\n down-sampling mask")
+
+        down = (1.0 / int(d))
+        zoom = [down, down, down]
+        mask = scipy.ndimage.interpolation.zoom(mask, zoom, order=0)
 
     masknii = nib.Nifti1Image(mask, mat)
     nib.save(masknii, outnii)
