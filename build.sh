a704aab243f6f9fafede0d30b8a3df55e581f79e
Jonas Osmann <j.osmann@mail.utoronto.ca>
2022-10-21 13:44:13 -0400

    [feat] add comments and checks to build script
    Adds additional comments to build script as well as additionally
    checking for docker-compose installation. If docker-compose is
    installed, instructions are given on how to run it. If it isn't
    installed the user is asked to install it first before running the image
    built with the build script.

--- build.sh
+++ build.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+# Check if Docker is installed
 if [ -x "$(command -v docker)" ]; then
 	    echo "Docker installation found. Building image."
 	    export HOST_USER=$(whoami)
 	    # Change user in docker-compose.yml to host user
 	    sed -i "s/\(\/home\/\).*\(\/.Xauthority:\/home\/\).*\(\/.Xauthority\)/\1$HOST_USER\2$HOST_USER\3/g" docker-compose.yml
+	    # Build MIRACL image from Dockerfile
+	    # Provide whoami, UID and GID
 	    docker build \
 	    --build-arg USER_ID=$(id -u) \
 	    --build-arg GROUP_ID=$(id -g) \
 	    --build-arg USER=$HOST_USER \
 	    -t mgoubran/miracl .
+	
+	    	# Test if docker-compose is installed
+		if [ -x "$(command -v docker-compose)" ]; then
+			echo "Run 'docker-compose up -d' to start the MIRACL container in background."
+		else
+			echo "docker-compose installation not found. Please install docker-compose to run the MIRACL container."
+		fi
 
 	else
 	    echo "Docker installation not found. Please install Docker first."
