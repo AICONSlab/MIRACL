b368608f3fd3bd691c25613d1ebc949cd67e8a03
Jonas Osmann <j.osmann@mail.utoronto.ca>
2022-11-03 16:53:52 -0400

    [feat] improve Docker image build script
    Adds description of script, functionality, copyright and current
    maintainer. Also adds basic flag functionality for -h,--help show usage
    and -l,--log for enabling logs for Docker build process. Introduces
    functionality to dynamically write user information into Dockerfile for
    use with Docker and Singularity. User information is deleted if build
    process fails or if build process completes to makes sure that it does
    not interfere with Dockerfile conversion to Singularity on CircleCI.

--- build.sh
+++ build.sh
@@ -1,53 +1,138 @@
 #!/bin/bash
 
-# Check if Docker is installed
-if [ -x "$(command -v docker)" ]; then
-	    echo
-	    echo "Docker installation found. Building image."
-	    echo
-	    export HOST_USER=$(whoami)
-	    # Change user in docker-compose.yml to host user
-	    sed -i "s/\(\/home\/\).*\(\/.Xauthority:\/home\/\).*\(\/.Xauthority\)/\1$HOST_USER\2$HOST_USER\3/g" docker-compose.yml
-	    # Build MIRACL image from Dockerfile
-	    # Provide whoami, UID and GID
-	    docker build \
-	    --build-arg USER_ID=$(id -u) \
-	    --build-arg GROUP_ID=$(id -g) \
-	    --build-arg USER=$HOST_USER \
-	    -t mgoubran/miracl .
+# Build MIRACL Docker image with user information from host system
+#
+# - Checks if Docker and Docker-Compose are installed
+# - Dynamically adds and removes user information to
+#   prevent errors in Singularity container creation
+# - Container user is the same as host user to prevent
+#   X11 issues with miraclGUI
+#
+# Current maintainer of script: Jonas Osmann @ github.com/jono3030
+# MIRACL (C) Maged Groubran @ maged.goubran@utoronto.ca
 
-	        build_status_code=$?
-		if [ $build_status_code -eq 0 ]; then
-			echo
-			echo "Build was successfull! Checking docker-compose installation."
-			echo
+# Script version
+version="1.1.1-beta"
 
-			# Test if docker-compose is installed
-			if [ -x "$(command -v docker-compose)" ]; then
-				echo "docker-compose installation found. Run 'docker-compose up -d' to start the MIRACL container in background."
-				echo
-			else
-				echo "docker-compose installation not found. Please install docker-compose to run the MIRACL container."
-				echo
-			fi
-		else
-			echo
-			echo "Build not successfull! An error occured with exit code: $build_status_code"
-			echo
-		fi
+# Set log variable
+write_log=false
 
-	else
-	    echo
-	    di_status_code=$?
-	    echo "Docker installation not found. Please install Docker first."
-	    echo "Exiting with status code $di_status_code"
-	    echo
+# Usage function
+function usage()
+{
+
+ cat <<usage
+
+ Automatically build MIRACL Docker image with pseudo host user
+
+ Version: $version
+
+ Usage: ./build.sh
+
+ Arguments:
+
+   -h,--help: Prints this help menu
+
+   -l, --log: Writes logfile of build process to build.log
+
+usage
+
+}
+
+# Basic flags function
+# Will be expanded in next iteration of MIRACL
+while [ ! $# -eq 0 ]
+do
+  if [ "$*" = "-h" ] || [ "$*" = "--help" ];
+  then
+    usage
+    exit
+  elif [ "$*" = "-l" ] || [ "$*" = "--log" ];
+  then
+    write_log=true
+  else
+    printf "\'$*\' is not a valid argument\n"
+    exit 1
+  fi
+  shift
+done 
+
+# Information that needs to be added to Dockerfile
+# to create pseudo host user. This is required to
+# make X11 work correclty with miraclGUI
+USER_TEXT=$(cat <<END
+# Setup host user as container user\n\
+ARG USER_ID=\$USER_ID\n\
+ARG GROUP_ID=\$GROUP_ID\n\
+ARG USER=\$USER\n\n\
+RUN addgroup --gid \$GROUP_ID \$USER\n\
+RUN adduser --disabled-password --gecos '' --uid \$USER_ID --gid \$GROUP_ID \$USER\n\n\
+# Change owner of /code directory\n\
+RUN chown -R \$USER:\$USER /code\n\
+# Change to \$USER\n\
+USER \$USER\n\
+WORKDIR /home/\$USER
+END
+)
+
+function rm_USER_TEXT () {
+  sed -i '/STARTUNCOMMENT/,/STOPUNCOMMENT/{//!d}' Dockerfile
+}
+
+# Check if $USER_TEXT strings are already present and delete if true
+if [ "$(sed -n '/#STARTUNCOMMENT#/{n;p;}' Dockerfile)" != "#STOPUNCOMMENT#" ]; then
+       	rm_USER_TEXT
 fi
 
+# Check if Docker is installed
+if [ -x "$(command -v docker)" ]; then
+	    printf "\nDocker installation found. Building image.\n"
+      # Adding host user requirements to Dockerfile
+      sed -i "/#STARTUNCOMMENT#/a $USER_TEXT" Dockerfile
+	    # Change user in docker-compose.yml to host user
+	    export HOST_USER=$(whoami)
+	    sed -i "s/\(\/home\/\).*\(\/.Xauthority:\/home\/\).*\(\/.Xauthority\)/\1$HOST_USER\2$HOST_USER\3/g" docker-compose.yml
+	    # Build MIRACL image from Dockerfile
+	    # Pass user name, UID and GID to Dockerfile
+      function docker_build () {
+	      docker build \
+	      --build-arg USER_ID=$(id -u) \
+	      --build-arg GROUP_ID=$(id -g) \
+	      --build-arg USER=$HOST_USER \
+	      -t mgoubran/miracl .
+      }
 
+      # Check for log flag
+      if [ $write_log = true ];
+      then
+        docker_build | tee build.log
+      else
+        docker_build
+      fi
 
+      # Check if build process exited without errors
+      build_status_code=$?
+      if [ $build_status_code -eq 0 ]; then
+        printf "\nBuild was successful! Checking docker-compose installation.\n"
+        # Remove $USER_TEXT from Dockerfile
+        rm_USER_TEXT
 
+        # Test if docker-compose is installed
+        # Should come by default with Docker-Desktop
+        if [ -x "$(command -v docker-compose)" ]; then
+          printf "Docker-Compose installation found. Run 'docker-compose up -d' to start the MIRACL container in background.\n"
+        else
+          printf "Docker-Compose installation not found. Please install docker-compose to run the MIRACL container.\n"
+        fi
+      else
+        # Return error code if build was not successful
+        printf "\nBuild not successful! An error occured with exit code: $build_status_code\n"
+        # Remove $USER_TEXT from Dockerfile
+        rm_USER_TEXT
+      fi
 
-
-
-
+else
+    di_status_code=$?
+    printf "\nDocker installation not found. Please install Docker first.\n"
+    printf "Exiting with status code $di_status_code\n"
+fi
