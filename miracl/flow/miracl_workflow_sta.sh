6ed943bd381bce22e851b1d1c7920f85cd6880dd
Jonas Osmann <j.osmann@mail.utoronto.ca>
2023-01-24 16:44:31 -0500

    [fix] add printing of chosen arguments to CLI
    Prints overview to the terminal of all arguments that were passed or
    assigned as default. This is useful for the user while running the STA
    flow in the CLI but also when checking the logs later.

--- miracl/flow/miracl_workflow_sta.sh
+++ miracl/flow/miracl_workflow_sta.sh
@@ -484,6 +484,32 @@ if [[ "$#" -gt 1 ]]; then
     lbl_mask=""
   fi
 
+  # Display provided/default arguments
+  printf "  \nThe following arguments will be used in the script:\n\n"
+  printf "  Input folder tifs: ${indir}\n"
+  printf "  Nifti folder: ${nii}\n"
+  printf "  Seed label abbreviation: ${lbl}\n"
+  printf "  Final registration folder: ${regdir}\n"
+  printf "  Hemisphere: ${hemi}\n"
+  printf "  Derivative of Gaussian: ${dog}\n"
+  printf "  Gaussian smooting sigma: ${gauss}\n"
+  printf "  Tracking angle threshold: ${angle}\n"
+  printf "  Downsample factor: ${down}\n"
+  printf "  Output channel name: ${chan}\n"
+  printf "  Output folder: ${out_dir}\n"
+  printf "  Brain mask: ${brain_mask}\n"
+  printf "  Seed mask: ${lbl_mask}\n"
+  printf "  Step length: ${step_length}\n"
+  printf "  Channel #: ${chann}\n"
+  printf "  Channel prefix: ${chanp}\n"
+  printf "  Original res x-y plane: ${vx}\n"
+  printf "  Original thickness: ${vz}\n"
+  printf "  Downsample in z: ${downz}\n"
+  printf "  Dilation factor for x: ${dilationfx}\n"
+  printf "  Dilation factor for y: ${dilationfy}\n"
+  printf "  Dilation factor for z: ${dilationfz}\n"
+  printf "  2nd order range-kutta method: ${rk2}\n\n"
+
 else
 
 	# call gui
