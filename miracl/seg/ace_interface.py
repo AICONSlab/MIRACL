f02d0b112ea5b6946f90e05669bd603b4877ac35
Jonas Osmann <j.osmann@mail.utoronto.ca>
2023-06-07 13:46:53 -0400

    [wip] 1st implementation prepare_model_transform
    Added config_unet.yml file. Refactored version of
    ace_prepare_model_transform.py. Implemented a check to test
    if CUDA is available. If it isn't, CPU will be loaded as the model
    device.

--- miracl/seg/ace_interface.py
+++ miracl/seg/ace_interface.py
@@ -1,10 +1,10 @@
-from miracl.seg import ace_generate_patch
+from miracl.seg import ace_generate_patch, ace_prepare_model_transform
 
 
 def main(args):
     # args_parser = aceParser()
     # args = args_parser.parse_args()
-    print("  The following parameters will be used:\n")
+    print("The following parameters will be used:\n")
     print(f"  Input file:       {args.input_folder}")
     print(f"  Output file:      {args.output_folder}")
     print(f"  Model type:       {args.model_type}")
@@ -19,8 +19,19 @@ def main(args):
     # print(f"In: {tmp_in}")
     # print(f"Out: {tmp_out}")
 
+    # INFO: ace_generate_patch.py
+
     # Pass input and output directory args to ace_generate_patch.py
-    ace_generate_patch.generate_patch_main(args.input_folder, args.output_folder)
+    # ace_generate_patch.generate_patch_main(
+    #         args.input_folder,
+    #         args.output_folder
+    #         )
+
+    # INFO: ace_prepare_model_transform.py
+
+    # Pass model type to ace_prepare_model_transform.py
+    model_out = ace_prepare_model_transform.generate_model_transforms(args.model_type)
+    # model_out, val_transforms = ace_prepare_model_transform.generate_model_transforms(args.model_type)
 
 
 if __name__ == "__main__":
--- miracl/seg/ace_prepare_model_transform.py
+++ miracl/seg/ace_prepare_model_transform.py
@@ -1,16 +1,13 @@
 """
 This code is written by Ahmadreza Attarpour, a.attarpour@mail.utoronto.ca
-this code uses MONAI and PyTorch for neuron segmentation project
-this the inference mode code when the model is trained and ready to use
+It uses MONAI and PyTorch for neuron segmentation project. The code inferences
+when the model is trained and ready to use.
 
-Inputs: 
-    Model name; it can be unet, unetr, or ensemble
-    a config file which is not defined by user
+Inputs:
+    Model name: {unet, unetr, ensemble}
+    A config file not defined by the user at a static path
 Outputs:
-    Pre-trained model(s)
-    Validation transforms 
-
-
+    tuple(Pre-trained model(s), Validation transforms)
 """
 # load libraries
 from monai.networks.nets import UNet, UNETR
@@ -19,122 +16,153 @@ from monai.transforms import (
     EnsureType,
     AddChannel,
     ScaleIntensityRangePercentiles,
-    Transform
+    Transform,
 )
 import torch
 from monai.networks.layers import Norm
 import numpy as np
 import yaml
-import tifffile 
+import tifffile
+import os
+from pathlib import Path
 
-# -------------------------------------------------------
-# inputs
-# -------------------------------------------------------
 
-model_name = ...
-cfg_path = ...
-    
-   
-# -------------------------------------------------------
-# load the config file
-# -------------------------------------------------------
+def generate_model_transforms(chosen_model):
+    # -------------------------------------------------------
+    # inputs
+    # -------------------------------------------------------
 
-def generate_model_transforms():
-    
+    # model_name = chosen_model
+    model_name = chosen_model
+    # print(os.getcwd())
+    # print(model_name)
+    cfg_path = Path(os.environ["MIRACL_HOME"]) / "seg/config_unetr.yml"
+    # print(f"cfg file path: {cfg_path}")
+
     # -------------------------------------------------------
     # load the config file
     # -------------------------------------------------------
 
     with open(cfg_path, "r") as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-    
+
+    # print(cfg)
+
+    # n_layers = cfg["unet"].get("n_encoder_layers", 5)
+    model_path_unet = cfg["general"].get("model_unet_trained_path")
+    model_path_unetr = cfg["general"].get("model_unetr_trained_path")
+
+    # print(f"# layers: {n_layers}")
+    # print(f"unet: {model_path_unet}")
+    # print(f"unetr: {model_path_unetr}")
+
+    if torch.cuda.is_available():
+        print("INFO: CUDA is available! Torch device will be set to `GPU`.\n")
+    else:
+        print("WARNING: CUDA is not available! Torch device will be set to 'CPU'.\n")
+
     # define the model
-    if model_name == 'unet':
-        crop_size = (128, 128, 128)
-        n_layers = cfg['unet'].get('n_encoder_layers', 5)
-        initial_channel = cfg['unet'].get('channels_initial', 16)
+    def unet_creator(cfg):
+        n_layers = cfg["unet"].get("n_encoder_layers", 5)
+        initial_channel = cfg["unet"].get("channels_initial", 16)
         model_unet = UNet(
             spatial_dims=3,
             in_channels=1,
             out_channels=2,
             channels=tuple([initial_channel * (2**j) for j in range(n_layers)]),
             strides=tuple([2] * (n_layers - 1)),
-            num_res_units=cfg['unet'].get('num_res_unit', 2),
-            dropout = cfg['unet'].get('dropout', 0.2),
-            norm = Norm.BATCH
+            num_res_units=cfg["unet"].get("num_res_unit", 2),
+            dropout=cfg["unet"].get("dropout", 0.2),
+            norm=Norm.BATCH,
         )
-    elif model_name == 'unetr':
+        return model_unet
+
+    def unetr_creator(cfg):
         crop_size = (96, 96, 96)
         model_unetr = UNETR(
             spatial_dims=3,
             in_channels=1,
             out_channels=2,
             img_size=crop_size,
-            feature_size=cfg['unetr'].get('feature_size', 16),
-            hidden_size=cfg['unetr'].get('hidden_size', 768),
-            mlp_dim=cfg['unetr'].get('mlp_dim', 3072),
-            num_heads=cfg['unetr'].get('num_heads', 12),
-            dropout_rate=cfg['unetr'].get('dropout', 0.2),
-            res_block=cfg['unetr'].get('res_block', True),
-            norm_name='batch'             
-            )
-    elif model_name == 'ensemble':     
-        # U-Net
-        crop_size = (128, 128, 128)
-        n_layers = cfg['unet'].get('n_encoder_layers', 5)
-        initial_channel = cfg['unet'].get('channels_initial', 16)
-        model_unet = UNet(
-            spatial_dims=3,
-            in_channels=1,
-            out_channels=2,
-            channels=tuple([initial_channel * (2**j) for j in range(n_layers)]),
-            strides=tuple([2] * (n_layers - 1)),
-            num_res_units=cfg['unet'].get('num_res_unit', 2),
-            dropout = cfg['unet'].get('dropout', 0.2),
-            norm = Norm.BATCH
+            feature_size=cfg["unetr"].get("feature_size", 16),
+            hidden_size=cfg["unetr"].get("hidden_size", 768),
+            mlp_dim=cfg["unetr"].get("mlp_dim", 3072),
+            num_heads=cfg["unetr"].get("num_heads", 12),
+            dropout_rate=cfg["unetr"].get("dropout", 0.2),
+            res_block=cfg["unetr"].get("res_block", True),
+            norm_name="batch",
         )
-        
-        # UNETR
-        crop_size = (96, 96, 96)
-        model_unetr = UNETR(
-            spatial_dims=3,
-            in_channels=1,
-            out_channels=2,
-            img_size=crop_size,
-            feature_size=cfg['unetr'].get('feature_size', 16),
-            hidden_size=cfg['unetr'].get('hidden_size', 768),
-            mlp_dim=cfg['unetr'].get('mlp_dim', 3072),
-            num_heads=cfg['unetr'].get('num_heads', 12),
-            dropout_rate=cfg['unetr'].get('dropout', 0.2),
-            res_block=cfg['unetr'].get('res_block', True),
-            norm_name='batch'             
-            )
-        
-    print("model(s) are initialized!")   
+        return model_unetr
+
+    model_path_unet = cfg["general"].get("model_unet_trained_path")
+    model_path_unetr = cfg["general"].get("model_unetr_trained_path")
+
+    if model_name == "unet":
+        model_unet = unet_creator(cfg)
+        if torch.cuda.is_available():
+            model_unet.load_state_dict(torch.load(model_path_unet))
+        else:
+            model_unet.load_state_dict(torch.load(
+                model_path_unet,
+                map_location='cpu'
+                ))
+        model_out = model_unet
+        print(f"Trained {model_name} model is initialized and loaded!\n")
+
+    elif model_name == "unetr":
+        model_unetr = unetr_creator(cfg)
+        if torch.cuda.is_available():
+            model_unetr.load_state_dict(torch.load(model_path_unetr))
+        else:
+            model_unetr.load_state_dict(torch.load(
+                model_path_unetr,
+                map_location='cpu'
+                ))
+        model_out = model_unetr
+        print(f"Trained {model_name} model is initialized and loaded!\n")
+
+    else:
+        model_unet, model_unetr = unet_creator(cfg), unetr_creator(cfg)
+        if torch.cuda.is_available():
+            model_unet.load_state_dict(torch.load(model_path_unet))
+            model_unetr.load_state_dict(torch.load(model_path_unetr))
+        else:
+            model_unet.load_state_dict(torch.load(
+                model_path_unet,
+                map_location='cpu'
+                ))
+            model_unetr.load_state_dict(torch.load(
+                model_path_unetr,
+                map_location='cpu'
+                ))
+        model_out = [model_unet, model_unetr]
+        print("unet and unetr models are initialized and loaded!\n")
+
+    # print("Trained model(s) are initialized and loaded!")
     # -------------------------------------------------------
     # load the model's weights
     # -------------------------------------------------------
-    model_path_unet = cfg['general'].get('model_unet_trained_path')
-    model_path_unetr = cfg['general'].get('model_unetr_trained_path')
-    
-    if model_name == 'unet':
-        model_unet.load_state_dict(torch.load(model_path_unet))
-        model_out = model_unet
-        
-    elif model_name == 'unetr':
-        model_unetr.load_state_dict(torch.load(model_path_unetr))
-        model_out = model_unetr
-        
-    elif model_name == 'ensemble':
-        model_unet.load_state_dict(torch.load(model_path_unet))
-        model_unetr.load_state_dict(torch.load(model_path_unetr))
-        model_out = [model_unet, model_unetr]
-    
-    print("Trained model(s) are loaded!")   
+    # model_path_unet = cfg["general"].get("model_unet_trained_path")
+    # model_path_unetr = cfg["general"].get("model_unetr_trained_path")
+
+    # if model_name == "unet":
+    #     model_unet.load_state_dict(torch.load(model_path_unet))
+    #     model_out = model_unet
+    #
+    # elif model_name == "unetr":
+    #     model_unetr.load_state_dict(torch.load(model_path_unetr))
+    #     model_out = model_unetr
+    #
+    # elif model_name == "ensemble":
+    #     model_unet.load_state_dict(torch.load(model_path_unet))
+    #     model_unetr.load_state_dict(torch.load(model_path_unetr))
+    #     model_out = [model_unet, model_unetr]
+
+    # print("Trained model(s) are loaded!")
     # -------------------------------------------------------
     # define transforms
     # -------------------------------------------------------
-    
+
     # return image_dict
     class MyLoadImage(Transform):
         def __call__(self, img_path):
@@ -143,23 +171,24 @@ def generate_model_transforms():
             # change the order axis of the image from DHW to HWD
             image = np.moveaxis(image, 0, 2)
             image = np.moveaxis(image, 0, 1)
-            
-            
             return image
-    
+
     val_transforms = Compose(
         [
             MyLoadImage(),
             AddChannel(),
-            ScaleIntensityRangePercentiles(keys=["image"], lower=0.05, upper=99.95, b_min=0, b_max=1, clip=True, relative=False),
+            ScaleIntensityRangePercentiles(
+                keys=["image"],
+                lower=0.05,
+                upper=99.95,
+                b_min=0,
+                b_max=1,
+                clip=True,
+                relative=False,
+            ),
             EnsureType(),
         ]
     )
-    
-    
+
     return model_out, val_transforms
-    
-
-
-
-    
+    # return model_out
