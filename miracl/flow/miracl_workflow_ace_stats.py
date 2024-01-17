78f7e5c5322f6521e6f6205d0ba3c3e51d8a0520
Anthony Rinaldi <anthony.rinaldi04@gmail.com>
2024-01-17 16:53:01 -0500

    [wip] Add ability for cluster method to glob match file names. Cluster method expects all warped nifti files in one dir, but the previous steps in the ace flow do not do this. We use an example nifti path and a base dir to glob match the rest of the warped nifti files.

--- miracl/flow/miracl_workflow_ace_stats.py
+++ miracl/flow/miracl_workflow_ace_stats.py
@@ -380,8 +380,17 @@ def main(args):
     startTime = datetime.now()
 
     # load wild images
-    wild_imgs = fnmatch.filter(os.listdir(wild_dir), "*.nii.gz")
-    wild_imgs.sort()
+    # TODO: fix --> do this based on downsample and resolution and the cluster directory
+    wild_warp_tiff_template = Path(wild_dir)
+    wild_base_dir = Path(args.wild[0])
+    wild_warp_tiff_extension = Path(
+        *wild_warp_tiff_template.relative_to(wild_base_dir).parts[1:]
+    )
+    wild_imgs_regex = "*/" + wild_warp_tiff_extension.as_posix()
+    wild_imgs = wild_base_dir.glob(wild_imgs_regex)
+
+    # wild_imgs = fnmatch.filter(os.listdir(wild_dir), "*.nii.gz")
+    # wild_imgs.sort()
 
     # only keep a quarter of an image for testing
     print("pre processing group 1 ...")
@@ -396,8 +405,16 @@ def main(args):
     print("found #", len(wild_imgs), "pre-processed wild type images!")
 
     # load dis images
-    dis_imgs = fnmatch.filter(os.listdir(dis_dir), "*.nii.gz")
-    dis_imgs.sort()
+    # TODO: fix --> do this based on downsample and resolution and the cluster directory
+    disease_warp_tiff_template = Path(dis_dir)
+    disease_base_dir = Path(args.disease[0])
+    disease_warp_tiff_extension = Path(
+        *disease_warp_tiff_template.relative_to(disease_base_dir).parts[1:]
+    )
+    dis_imgs_regex = "*/" + disease_warp_tiff_extension.as_posix()
+    dis_imgs = disease_base_dir.glob(dis_imgs_regex)
+    # dis_imgs = fnmatch.filter(os.listdir(dis_dir), "*.nii.gz")
+    # dis_imgs.sort()
 
     # only keep a quarter of an image for testing
     print("pre processing group 2 ...")
