a6f11761bea1f6ad3778641ed18a9c3f0f842f0f
Jonas Osmann <j.osmann@mail.utoronto.ca>
2024-07-22 13:50:06 -0400

    [wip] further implement save/load functionality

--- miracl/system/io_utils.py
+++ miracl/system/io_utils.py
@@ -4,37 +4,36 @@ from datetime import datetime
 
 
 class UserInputPairsManager:
-    def __init__(self, window_name):
-        self.window_name = window_name
-        self.unique_id = str(uuid.uuid4())
-        self.last_saved = None
-        self.last_loaded = None
+    def __init__(self, module_name):
+        self.metadata = {
+            "uuid": str(uuid.uuid4()),
+            "module_name": module_name,
+            "last_saved": None,
+            "last_loaded": None,
+        }
         self.user_input_pairs = {}
 
     def set_user_input_pairs(self, user_input_pairs):
         self.user_input_pairs = user_input_pairs
 
     def save_to_json(self, file_path):
+        self.metadata["last_saved"] = str(datetime.now())
         data = {
-            "unique_id": self.unique_id,
-            "window_name": self.window_name,
-            "last_saved": str(datetime.now()),
-            "last_loaded": str(self.last_loaded) if self.last_loaded else None,
+            "metadata": self.metadata,
             "user_input_pairs": self.user_input_pairs,
         }
 
         with open(file_path, "w") as file:
             ujson.dump(data, file, indent=4)
 
-        self.last_saved = datetime.now()
-
     def load_from_json(self, file_path):
         with open(file_path, "r") as file:
             loaded_data = ujson.load(file)
 
-        self.__dict__.update(loaded_data)  # Update instance var
+        self.metadata = loaded_data.get("metadata", {})
+        self.user_input_pairs = loaded_data.get("user_input_pairs", {})
 
-        loaded_data["last_loaded"] = str(datetime.now())
+        self.metadata["last_loaded"] = str(datetime.now())
 
         with open(file_path, "w") as file:
             ujson.dump(loaded_data, file, indent=4)
