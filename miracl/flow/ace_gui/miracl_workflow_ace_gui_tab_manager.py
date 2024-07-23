1e4f8fc11f46898afeeb308911ca5a7936fad0f6
Jonas Osmann <j.osmann@mail.utoronto.ca>
2024-07-23 17:49:11 -0400

    [fix] correct incorrect reference path that caused cli error

--- miracl/flow/ace_gui/miracl_workflow_ace_gui_tab_manager.py
+++ miracl/flow/ace_gui/miracl_workflow_ace_gui_tab_manager.py
@@ -18,14 +18,26 @@ License:
 """
 
 from PyQt5.QtWidgets import QTabWidget
-from tabs.miracl_workflow_ace_gui_main_tab import MainTab
-from tabs.miracl_workflow_ace_gui_segmentation_tab import SegmentationTab
-from tabs.miracl_workflow_ace_gui_clarity_registration_tab import ClarityRegistrationTab
-from tabs.miracl_workflow_ace_gui_voxelizing_warping_tab import VoxelizingWarpingTab
-from tabs.miracl_workflow_ace_gui_conversion_tab import ConversionTab
-from tabs.miracl_workflow_ace_gui_clusterwise_tab import ClusterwiseTab
-from tabs.miracl_workflow_ace_gui_correlation_stats_tab import CorrelationStatsTab
-from tabs.miracl_workflow_ace_gui_heatmap_tab import HeatmapTab
+from miracl.flow.ace_gui.tabs.miracl_workflow_ace_gui_main_tab import MainTab
+from miracl.flow.ace_gui.tabs.miracl_workflow_ace_gui_segmentation_tab import (
+    SegmentationTab,
+)
+from miracl.flow.ace_gui.tabs.miracl_workflow_ace_gui_clarity_registration_tab import (
+    ClarityRegistrationTab,
+)
+from miracl.flow.ace_gui.tabs.miracl_workflow_ace_gui_voxelizing_warping_tab import (
+    VoxelizingWarpingTab,
+)
+from miracl.flow.ace_gui.tabs.miracl_workflow_ace_gui_conversion_tab import (
+    ConversionTab,
+)
+from miracl.flow.ace_gui.tabs.miracl_workflow_ace_gui_clusterwise_tab import (
+    ClusterwiseTab,
+)
+from miracl.flow.ace_gui.tabs.miracl_workflow_ace_gui_correlation_stats_tab import (
+    CorrelationStatsTab,
+)
+from miracl.flow.ace_gui.tabs.miracl_workflow_ace_gui_heatmap_tab import HeatmapTab
 
 
 class TabManager:
