1c37a3b9742327e2d9e4061370372646991356f1
Jonas Osmann <j.osmann@alumni.utoronto.ca>
2024-12-06 13:53:48 -0500

    [feat] start refactoring install.sh to prep for updated functionality
    install.sh needs to be updated with new functionality based on user
    feedback. Before implementing the user feedback though it is useful to
    first refactor the script. This will make it easier to implement the new
    features.

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@
 #   prevent errors in Singularity container creation
 # - Container user is the same as host user to prevent
 #   X11 issues with miraclGUI
+# - Checks if script is run with 'sudo'
 
 # Current maintainer of script: Jonas Osmann @ github.com/jono3030
 # MIRACL (C) Maged Groubran @ maged.goubran@utoronto.ca
@@ -18,6 +19,9 @@
 # Set variables base state
 # Detect OS (Linux/MacOs)
 os=$(uname -s)
+
+# Detect username
+SUDO_USER_CHECK=${SUDO_USER:-$USER}
 
 # Script version
 version="2.0.1-beta"
@@ -39,12 +43,12 @@ miracl_version_file=$(cat ./miracl/version.txt)
 
 # Set container memory size limit
 if [[ "$os" == "Linux" ]]; then
-    shm_mem=$(grep MemTotal /proc/meminfo | awk '{printf "%dmb", int($2/1024*0.85)}')
+  shm_mem=$(grep MemTotal /proc/meminfo | awk '{printf "%dmb", int($2/1024*0.85)}')
 elif [[ "$os" == "Darwin" ]]; then
-    shm_mem=$(sysctl hw.memsize | awk '{printf "%dmb", int($2/1024/1024*0.85)}')
+  shm_mem=$(sysctl hw.memsize | awk '{printf "%dmb", int($2/1024/1024*0.85)}')
 else
-    echo "Unsupported operating system: $os"
-    exit 1
+  echo "Unsupported operating system: $os"
+  exit 1
 fi
 
 # Set array to capture volumes
@@ -58,10 +62,9 @@ base_usage="Usage: ./$(basename "$0") [-n service_name] [-i image_name] [-c cont
 ##########
 
 # Usage function
-function usage()
-{
+function usage() {
 
- cat <<usage
+  cat <<usage
 
  $base_usage
 
@@ -89,25 +92,27 @@ usage
 
 }
 
-# Parse command line arguments
-while getopts ":n:i:c:t:ged:v:lsmh" opt; do
-  case ${opt} in
+# Args parser
+function parse_args() {
+  # Parse command line arguments
+  while getopts ":n:i:c:t:ged:v:lsmh" opt; do
+    case ${opt} in
 
     n)
       if [ "${OPTARG}" != "$service_name" ]; then
-      service_name=${OPTARG}
+        service_name=${OPTARG}
       fi
       ;;
-     
+
     i)
       if [ "${OPTARG}" != "$image_name" ]; then
-      image_name=${OPTARG}
+        image_name=${OPTARG}
       fi
       ;;
 
     c)
       if [ "${OPTARG}" != "$container_name" ]; then
-      container_name=${OPTARG}
+        container_name=${OPTARG}
       fi
       ;;
 
@@ -132,7 +137,7 @@ while getopts ":n:i:c:t:ged:v:lsmh" opt; do
 
     d)
       if [ "${OPTARG}" != "$shm_mem" ]; then
-      shm_mem=${OPTARG}
+        shm_mem=${OPTARG}
       fi
       ;;
 
@@ -173,14 +178,22 @@ while getopts ":n:i:c:t:ged:v:lsmh" opt; do
 
     esac
   done
-  shift "$((OPTIND -1))"
+  shift "$((OPTIND - 1))"
+}
+
+parse_args "$@"
+
+echo "This is service_name: $service_name"
+echo "This is image_name: $image_name"
+echo "This is container_name: $container_name"
+exit 1
 
 ##################
 # DOCKER COMPOSE #
 ##################
 
 # Generate docker-compose.yml file
-cat > docker-compose.yml <<EOF
+cat >docker-compose.yml <<EOF
 version: "3.3"
 services:
   $service_name:
@@ -196,7 +209,7 @@ EOF
 
 if [[ $gpu ]]; then
 
-cat >> docker-compose.yml <<EOF
+  cat >>docker-compose.yml <<EOF
     deploy:
       resources:
         reservations:
@@ -209,7 +222,7 @@ EOF
 fi
 
 # Append required volumes
-cat >> docker-compose.yml <<EOF
+cat >>docker-compose.yml <<EOF
     volumes:
       - /home/josmann/.Xauthority:/home/josmann/.Xauthority
 EOF
@@ -217,9 +230,9 @@ EOF
 # Append MIRACL scripts folder mounting
 if [[ -z $dev ]]; then
 
-install_script_dir=$(dirname "$(readlink -f "$0")")
+  install_script_dir=$(dirname "$(readlink -f "$0")")
 
-cat >> docker-compose.yml <<EOF
+  cat >>docker-compose.yml <<EOF
       - ${install_script_dir}/miracl:/code/miracl
 EOF
 
@@ -227,7 +240,7 @@ fi
 
 # Add additional volumes
 for v in "${volumes[@]}"; do
-  echo "      - $v" >> docker-compose.yml
+  echo "      - $v" >>docker-compose.yml
 done
 
 ##############
@@ -237,7 +250,8 @@ done
 # Information that needs to be added to Dockerfile
 # to create pseudo host user. This is required to
 # make X11 work correclty with miraclGUI
-USER_TEXT=$(cat <<END
+USER_TEXT=$(
+  cat <<END
 # Setup host user as container user\n\
 ARG USER_ID=\$USER_ID\n\
 ARG GROUP_ID=\$GROUP_ID\n\
@@ -253,13 +267,13 @@ END
 )
 
 # Function to remove user text in Dockerfile if it is already present
-function rm_USER_TEXT () {
+function rm_USER_TEXT() {
   sed -i '/STARTUNCOMMENT/,/STOPUNCOMMENT/{//!d}' Dockerfile
 }
 
 # Check if $USER_TEXT strings are already present and delete if true
 if [ "$(sed -n '/#STARTUNCOMMENT#/{n;p;}' Dockerfile)" != "#STOPUNCOMMENT#" ]; then
-       	rm_USER_TEXT
+  rm_USER_TEXT
 fi
 
 #########
@@ -268,80 +282,79 @@ fi
 
 # Check if Docker is installed
 if [ -x "$(command -v docker)" ]; then
-	    printf "\nDocker installation found.\n"
-      # Adding host user requirements to Dockerfile
-      sed -i "/#STARTUNCOMMENT#/a $USER_TEXT" Dockerfile
-	    # Change user in docker-compose.yml to host user
-	    HOST_USER=$(whoami)
-      export HOST_USER
-	    sed -i "s/\(\/home\/\).*\(\/.Xauthority:\/home\/\).*\(\/.Xauthority\)/\1$HOST_USER\2$HOST_USER\3/g" docker-compose.yml
+  printf "\nDocker installation found.\n"
+  # Adding host user requirements to Dockerfile
+  sed -i "/#STARTUNCOMMENT#/a $USER_TEXT" Dockerfile
+  # Change user in docker-compose.yml to host user
+  HOST_USER=$(whoami)
+  export HOST_USER
+  sed -i "s/\(\/home\/\).*\(\/.Xauthority:\/home\/\).*\(\/.Xauthority\)/\1$HOST_USER\2$HOST_USER\3/g" docker-compose.yml
 
-      ######################################
-	    # Build MIRACL image from Dockerfile #
-      ######################################
-      
-      # Printing information about build process and docker-compose.yml to stdout
-      printf "\n[+] Building MIRACL and creating docker-compose.yml with the following parameters:\n"
-      printf " User: %s\n" "$HOST_USER"
-      printf " pid: %s\n" "$(id -u)"
-      printf " gid: %s\n" "$(id -g)"
-      printf " Max shared memory: %s\n" "$shm_mem"
-      printf " Service name: %s\n" "$service_name"
-      printf " Image name: %s\n" "$image_name:$miracl_version"
-      printf " Container name: %s\n" "$container_name"
-      printf " GPU passthrough: %s\n" "${gpu:-false}"
-      printf " Script dir mounted: %s\n" "${dev:-true}"
-      printf " Log file: %s\n" "${write_log:-false}"
-      for v in "${!volumes[@]}"; do
-        printf " Volume %s: %s\n" "$v" "${volumes[$v]}"
-      done
+  ######################################
+  # Build MIRACL image from Dockerfile #
+  ######################################
 
-	    # Pass user name, UID and GID to Dockerfile
-      function docker_build () {
-	      docker build \
-	      --build-arg USER_ID="$(id -u)" \
-	      --build-arg GROUP_ID="$(id -g)" \
-	      --build-arg USER="$HOST_USER" \
-	      -t "$image_name":"$miracl_version" .
-      }
+  # Printing information about build process and docker-compose.yml to stdout
+  printf "\n[+] Building MIRACL and creating docker-compose.yml with the following parameters:\n"
+  printf " User: %s\n" "$HOST_USER"
+  printf " pid: %s\n" "$(id -u)"
+  printf " gid: %s\n" "$(id -g)"
+  printf " Max shared memory: %s\n" "$shm_mem"
+  printf " Service name: %s\n" "$service_name"
+  printf " Image name: %s\n" "$image_name:$miracl_version"
+  printf " Container name: %s\n" "$container_name"
+  printf " GPU passthrough: %s\n" "${gpu:-false}"
+  printf " Script dir mounted: %s\n" "${dev:-true}"
+  printf " Log file: %s\n" "${write_log:-false}"
+  for v in "${!volumes[@]}"; do
+    printf " Volume %s: %s\n" "$v" "${volumes[$v]}"
+  done
 
-      # Check for log flag
-      if [ $write_log ];
-      then
-        docker_build | tee build.log
-      else
-        docker_build
-      fi
+  # Pass user name, UID and GID to Dockerfile
+  function docker_build() {
+    docker build \
+      --build-arg USER_ID="$(id -u)" \
+      --build-arg GROUP_ID="$(id -g)" \
+      --build-arg USER="$HOST_USER" \
+      -t "$image_name":"$miracl_version" .
+  }
 
-      # Check if build process exited without errors
-      build_status_code=$?
-      if [ $build_status_code -eq 0 ]; then
-        printf "\nBuild was successful! Checking Docker Compose installation.\n"
-        # Remove $USER_TEXT from Dockerfile
-        rm_USER_TEXT
+  # Check for log flag
+  if [ "${write_log}" ]; then
+    docker_build | tee build.log
+  else
+    docker_build
+  fi
 
-        # Test if docker-compose is installed
-        # Should come by default with Docker-Desktop
-        if [ -x "$(command -v docker-compose)" ]; then
-          printf "Docker Compose installation found (%s). Run 'docker-compose up -d' to start the container in the background and then run 'docker exec -it ${container_name} bash' to enter the container.\n" "$(docker-compose --version)"
-        elif dcex=$(docker compose version); then
-          printf "Docker Compose installation found (%s). Run 'docker compose up -d' or use Docker Desktop (if installed) to start the container in the background and then run 'docker exec -it ${container_name} bash' to enter the container.\n" "$dcex"
-        else
-          printf "Docker Compose installation not found. Please install Docker Compose plugin or standalone version to run the MIRACL container. Instructions on how to install Docker Compose can be found here: https://docs.docker.com/compose/install/\n"
-        fi
-      else
-        # Return error code if build was not successful
-        printf "\nBuild not successful! An error occured with exit code: %s\n" $build_status_code
-        # Remove $USER_TEXT from Dockerfile
-        rm_USER_TEXT
-        # Exit with custom status code
-        exit $build_status_code
-      fi
+  # Check if build process exited without errors
+  build_status_code=$?
+  if [ $build_status_code -eq 0 ]; then
+    printf "\nBuild was successful! Checking Docker Compose installation.\n"
+    # Remove $USER_TEXT from Dockerfile
+    rm_USER_TEXT
+
+    # Test if docker-compose is installed
+    # Should come by default with Docker-Desktop
+    if [ -x "$(command -v docker-compose)" ]; then
+      printf "Docker Compose installation found (%s). Run 'docker-compose up -d' to start the container in the background and then run 'docker exec -it ${container_name} bash' to enter the container.\n" "$(docker-compose --version)"
+    elif dcex=$(docker compose version); then
+      printf "Docker Compose installation found (%s). Run 'docker compose up -d' or use Docker Desktop (if installed) to start the container in the background and then run 'docker exec -it ${container_name} bash' to enter the container.\n" "$dcex"
+    else
+      printf "Docker Compose installation not found. Please install Docker Compose plugin or standalone version to run the MIRACL container. Instructions on how to install Docker Compose can be found here: https://docs.docker.com/compose/install/\n"
+    fi
+  else
+    # Return error code if build was not successful
+    printf "\nBuild not successful! An error occured with exit code: %s\n" $build_status_code
+    # Remove $USER_TEXT from Dockerfile
+    rm_USER_TEXT
+    # Exit with custom status code
+    exit $build_status_code
+  fi
 
 else
-    di_status_code=$?
-    printf "\nDocker installation not found. Please install Docker first.\n"
-    printf "Exiting with status code %s\n" $di_status_code
-    # Exit with custom status code
-    exit $di_status_code
+  di_status_code=$?
+  printf "\nDocker installation not found. Please install Docker first.\n"
+  printf "Exiting with status code %s\n" $di_status_code
+  # Exit with custom status code
+  exit $di_status_code
 fi
