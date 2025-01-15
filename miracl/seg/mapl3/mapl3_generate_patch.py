08c64fc43677bfebf045edd83a783c5430980c76
Jonas Osmann <j.osmann@alumni.utoronto.ca>
2025-01-15 15:54:05 -0500

    [fix] uncomment erroniously commented section

--- miracl/seg/mapl3/mapl3_generate_patch.py
+++ miracl/seg/mapl3/mapl3_generate_patch.py
@@ -47,6 +47,7 @@ logger = miracl_logger.logger
 # -------------------------------------------------------
 # my_parser = argparse.ArgumentParser(description="Working directory")
 
+
 # Add the arguments
 # my_parser.add_argument(
 #     "-i",
@@ -83,24 +84,24 @@ logger = miracl_logger.logger
 # )
 #
 #
-# # -------------------------------------------------------
-# # function for windowing
-# # -------------------------------------------------------
-# def blockshaped(arr, nrows, ncols):
-#     """
-#     Return an array of shape (n, nrows, ncols) where
-#     n * nrows * ncols = arr.size
-#     If arr is a 2D array, the returned array should look like n subblocks with
-#     each subblock preserving the "physical" layout of arr.
-#     """
-#     h, w = arr.shape
-#     assert h % nrows == 0, "{} rows is not evenly divisble by {}".format(h, nrows)
-#     assert w % ncols == 0, "{} cols is not evenly divisble by {}".format(w, ncols)
-#     return (
-#         arr.reshape(h // nrows, nrows, -1, ncols)
-#         .swapaxes(1, 2)
-#         .reshape(-1, nrows, ncols)
-#     )
+# -------------------------------------------------------
+# function for windowing
+# -------------------------------------------------------
+def blockshaped(arr, nrows, ncols):
+    """
+    Return an array of shape (n, nrows, ncols) where
+    n * nrows * ncols = arr.size
+    If arr is a 2D array, the returned array should look like n subblocks with
+    each subblock preserving the "physical" layout of arr.
+    """
+    h, w = arr.shape
+    assert h % nrows == 0, "{} rows is not evenly divisble by {}".format(h, nrows)
+    assert w % ncols == 0, "{} cols is not evenly divisble by {}".format(w, ncols)
+    return (
+        arr.reshape(h // nrows, nrows, -1, ncols)
+        .swapaxes(1, 2)
+        .reshape(-1, nrows, ncols)
+    )
 
 
 # -------------------------------------------------------
