cef31d326afc00fcabb173b59af3deb8a10d923b
Jonas Osmann <j.osmann@alumni.utoronto.ca>
2025-10-13 14:42:34 -0400

    [fix] attr id should be of type UUID not str

--- miracl/system/datamodels/datamodel_miracl_objs.py
+++ miracl/system/datamodels/datamodel_miracl_objs.py
@@ -28,6 +28,7 @@ from miracl.system.enums.enums_base_modules import (
 from argparse import ArgumentTypeError
 from pathlib import Path
 import re
+from uuid import UUID
 
 ############
 # UTIL FNS #
@@ -97,7 +98,7 @@ class LineEditConfig(BaseModel):
 
 class MiraclObj(BaseModel):
     # REQUIRED FIELDS
-    id: str = Field(
+    id: UUID = Field(
         ...,
         description="Unique id for object",
         example="67b62f10-a6b6-4d61-9e2c-19819373265d",
