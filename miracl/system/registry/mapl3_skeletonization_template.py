f270bd0465753403cd4b363b9925950b91407faa
Jonas Osmann <j.osmann@alumni.utoronto.ca>
2025-10-21 16:01:41 -0400

    [fix] create a separate Conda env for skeletonization, install required packages and call Python env bin directly from the runner
    mapl3-env.yaml:
    ```
    name: mapl3-env
    channels:
      - defaults
    dependencies:
      - python=3.10.8
      - pip
      - pip:
          - annotated-types==0.7.0
          - click==8.3.0
          - cucim-cu12==25.10.0
          - cupy-cuda12x==13.6.0
          - fastrlock==0.8.3
          - imageio==2.37.0
          - joblib==1.5.2
          - lazy_loader==0.4
          - MIRACL==2.4.0
          - networkx==3.4.2
          - numpy==2.2.6
          - packaging==25.0
          - pandas==2.3.3
          - pillow==12.0.0
          - pydantic==2.12.3
          - pydantic_core==2.41.4
          - python-dateutil==2.9.0.post0
          - pytz==2025.2
          - scikit-image==0.25.2
          - scipy==1.15.3
          - six==1.17.0
          - tifffile==2025.5.10
          - typing-inspection==0.4.2
          - typing_extensions==4.15.0
          - tzdata==2025.2
    ```

--- miracl/system/registry/mapl3_skeletonization_template.py
+++ miracl/system/registry/mapl3_skeletonization_template.py
@@ -4,7 +4,7 @@ from miracl.system.objs.objs_seg.objs_mapl3.objs_mapl3_skeletonization import (
 from miracl.system.datamodels.miraclobj_enums import ModuleType
 
 mapl3_skeletonization_dict = {
-    "script": "python /code/miracl/seg/mapl3/mapl3_skeletonization.py",
+    "script": "/home/josmann/.conda/envs/mapl3-env/bin/python /code/miracl/seg/mapl3/mapl3_skeletonization.py",
     "obj_class": Skeletonization,
     "module_type": ModuleType.FLOW_MAPL3,
 }
