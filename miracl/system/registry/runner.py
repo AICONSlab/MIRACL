ad7cabeafe052184019c2a9d1ef3d4814186cc9a
Jonas Osmann <j.osmann@alumni.utoronto.ca>
2025-10-21 00:41:12 -0400

    [fix] fix runner not returning value if None

--- miracl/system/registry/runner.py
+++ miracl/system/registry/runner.py
@@ -4,8 +4,8 @@ from typing import Optional, Dict
 def default_runner(script: str, mapping: Dict[str, Optional[str]]):
     parts = [script]
     for flag, value in mapping.items():
-        if value is not None:
-            parts.append(f"{flag} {value}")
+        # if value is not None:
+        parts.append(f"{flag} {value}")
     cmd_str = " ".join(map(str, parts))
     print(f"Running: {cmd_str}")
     # subprocess.run(cmd_str, shell=True)
