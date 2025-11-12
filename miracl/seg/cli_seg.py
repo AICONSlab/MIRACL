f7e0511d9d8621f8f5943ccfd1e18a56662149ca
Jonas Osmann <j.osmann@alumni.utoronto.ca>
2025-11-12 14:29:57 -0500

    [wip] remove old MAPL3 workflow from cli scripts

--- miracl/seg/cli_seg.py
+++ miracl/seg/cli_seg.py
@@ -3,8 +3,8 @@ import os
 import subprocess
 import sys
 
-from miracl.seg.mapl3 import mapl3_cli_parser
-from miracl.seg.mapl3 import mapl3_main
+# from miracl.seg.mapl3 import mapl3_cli_parser
+# from miracl.seg.mapl3 import mapl3_main
 
 from miracl.seg import (
     ace_interface,
@@ -60,9 +60,9 @@ def run_voxelize(parser, args):
     miracl_seg_voxelize_parallel.main(args)
 
 
-def run_mapl3(parser, args):
-    sys.argv = sys.argv[2:]
-    mapl3_main.main()
+# def run_mapl3(parser, args):
+#     sys.argv = sys.argv[2:]
+#     mapl3_main.main()
 
 
 def run_ace(parser, args):
@@ -133,18 +133,18 @@ def get_parser():
 
     parser_voxelize.set_defaults(func=run_voxelize)
 
-    # MAPL3
-    mapl3_parser_class = mapl3_cli_parser.Mapl3Parser()
-    mapl3_parsefn = mapl3_parser_class.parsefn()
-    parser_mapl3 = subparsers.add_parser(
-        "mapl3",
-        parents=[mapl3_parsefn],
-        add_help=False,
-        usage=mapl3_parsefn.usage,
-        help="mapping axonal projection in light sheet microscopy in 3d",
-    )
-
-    parser_mapl3.set_defaults(func=run_mapl3)
+    # # MAPL3
+    # mapl3_parser_class = mapl3_cli_parser.Mapl3Parser()
+    # mapl3_parsefn = mapl3_parser_class.parsefn()
+    # parser_mapl3 = subparsers.add_parser(
+    #     "mapl3",
+    #     parents=[mapl3_parsefn],
+    #     add_help=False,
+    #     usage=mapl3_parsefn.usage,
+    #     help="mapping axonal projection in light sheet microscopy in 3d",
+    # )
+    #
+    # parser_mapl3.set_defaults(func=run_mapl3)
 
     # ace
     ace_parser_class = ace_parser.aceParser()
