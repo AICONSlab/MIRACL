570186eaf841b03d4f39b15fa86034aab4e407e2
Jonas Osmann <j.osmann@alumni.utoronto.ca>
2025-11-24 12:28:35 -0500

    [fix] fix formatting

--- miracl/__init__.py
+++ miracl/__init__.py
@@ -9,7 +9,17 @@ import miracl.sta
 import miracl.stats
 import miracl.utilfn
 
-__all__ = ['connect', 'flow', 'conv', 'lbls', 'reg', 'seg', 'sta', 'stats', 'utilfn']
+__all__ = [
+    "connect",
+    "flow",
+    "conv",
+    "lbls",
+    "reg",
+    "seg",
+    "sta",
+    "stats",
+    "utilfn",
+]
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DEPENDS_DIR = os.path.abspath(os.path.join(ROOT_DIR, "..", "depends"))
