4e0648ed9a3ec04db7bf76141d8dccaf8e14a520
Jonas Osmann <j.osmann@alumni.utoronto.ca>
2025-11-24 16:48:21 -0500

    [feat] import parser_true_or_false from miracl.api.utils
    [fix] remove obsolete code

--- miracl/system/datamodels/datamodel_miracl_objs.py
+++ miracl/system/datamodels/datamodel_miracl_objs.py
@@ -18,72 +18,21 @@ from pydantic import (
     FieldValidationInfo,
     field_validator,
 )
-
 from miracl.api.enums import (
-    ModuleType,
     ArgumentType,
     ArgumentAction,
     WidgetType,
     InputRestrictionType,
     CliGroup,
 )
-# from miracl.system.datamodels.miraclobj_enums import (
-#     ModuleType,
-#     ArgumentType,
-#     ArgumentAction,
-#     WidgetType,
-#     InputRestrictionType,
-# )
-#
-# from miracl.system.enums.enums_base_modules import (
-#     CliGroup,
-# )
-
+from miracl.api.utils import (
+    parser_true_or_false,
+)
 from argparse import ArgumentTypeError
 from pathlib import Path
 import re
 from uuid import UUID, uuid4
 import threading
-
-############
-# UTIL FNS #
-############
-
-
-def parser_true_or_false(arg: str) -> bool:
-    """
-    Convert a string argument to a boolean value.
-
-    Accepts "true", "t" (case-insensitive) as True;
-    "false", "f" as False. Raises an error for anything else.
-
-    Args:
-        arg (str): Input string to convert.
-
-    Returns:
-        bool: True or False
-
-    Raises:
-        ArgumentTypeError: If input is not one of the accepted values.
-    """
-    upper_arg = str(arg).upper()
-    if upper_arg in ("TRUE", "T"):
-        return True
-    elif upper_arg in ("FALSE", "F"):
-        return False
-    else:
-        raise ArgumentTypeError("Argument must be either 'True'/'T' or 'False'/'F'")
-
-
-# def parser_true_or_false(arg: str) -> bool:
-#     upper_arg = str(arg).upper()
-#     if upper_arg in ("TRUE", "T"):
-#         return True
-#     elif upper_arg in ("FALSE", "F"):
-#         return False
-#     else:
-#         raise ArgumentTypeError("Argument must be either 'True'/'T' or 'False'/'F'")
-
 
 #################
 # CUSTOM FIELDS #
