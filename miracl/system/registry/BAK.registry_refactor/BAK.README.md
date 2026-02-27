d25c46f744e60d86d360ca3e7ad458b63e53dd1d
Jonas Osmann <j.osmann@alumni.utoronto.ca>
2026-02-27 18:55:10 -0500

    [wip] delete obsolete, dev versions

--- miracl/system/registry/BAK.registry_refactor/BAK.README.md
+++
@@ -1,9 +0,0 @@
-# Notes
-
-## CLI-as-API design
-
-### Execution
-**Introspector** ("Librarian"): Finds data.
-**Serializer** ("Translator"): Formats data.
-**Executor** ("Boss"): Decides what to do.
-**Runner** ("Courier"): Delivers the data.
--- miracl/system/registry/BAK.registry_refactor/BAK.executor.py
+++
@@ -1,9 +0,0 @@
-from miracl.system.registry.registry_refactor.datamodels import CommandPlan
-
-
-class MiraclExecutor:
-    @staticmethod
-    def execute(execution_plans: CommandPlan):
-        for plan in execution_plans:
-            print(f"Module:                 {plan.module_name}")
-            plan.runner(plan.tokens, plan.execute)
