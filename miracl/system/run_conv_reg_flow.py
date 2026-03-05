d01c8379f8054df01a6bd3d9ea1abeeba69d3d62
Jonas Osmann <j.osmann@alumni.utoronto.ca>
2026-03-05 12:46:44 -0500

    [wip] minor additions and testing

--- miracl/system/run_conv_reg_flow.py
+++ miracl/system/run_conv_reg_flow.py
@@ -40,31 +40,31 @@ cli_builder = MiraclCLIBuilder()
 _ = cli_builder.build_parser(cli_builder_parser_input)
 # Parse args
 parsed_args = cli_builder.parse()
-#
-# populated_modules = deserialize_parsed_args_to_objects(
-#     parsed_args=parsed_args,
-#     resolved_objects=module_objects,
-#     strict=False,
-# )
-#
-# # Initialize CLI command builder
-# cli_plan_builder = CLICommandBuilder()
-# # Initialize workflow orchestrator with CLI command builder
-# cli_orchestrator = WorkflowOrchestrator(builder=cli_plan_builder)
-# # Generate CLI commands for executor
-# cli_plans = cli_orchestrator.generate_plans(
-#     parsed_module_objects=populated_modules,
-#     parsed_registry_metadata=registry_metadata,
-#     workflow_config=workflow,
-# )
-#
-# # Initialize namespace command builder
-# namespace_plan_builder = PythonNamespaceBuilder()
-# # Initialize workflow orchestrator with namespace command builder
-# namespace_orchestrator = WorkflowOrchestrator(builder=namespace_plan_builder)
-# # Generate namespace commands for executor
-# namespace_plans = namespace_orchestrator.generate_plans(
-#     parsed_module_objects=populated_modules,
-#     parsed_registry_metadata=registry_metadata,
-#     workflow_config=workflow,
-# )
+
+populated_modules = deserialize_parsed_args_to_objects(
+    parsed_args=parsed_args,
+    resolved_objects=module_objects,
+    strict=False,
+)
+
+# Initialize CLI command builder
+cli_plan_builder = CLICommandBuilder()
+# Initialize workflow orchestrator with CLI command builder
+cli_orchestrator = WorkflowOrchestrator(builder=cli_plan_builder)
+# Generate CLI commands for executor
+cli_plans = cli_orchestrator.generate_plans(
+    parsed_module_objects=populated_modules,
+    parsed_registry_metadata=registry_metadata,
+    workflow_config=workflow,
+)
+
+# Initialize namespace command builder
+namespace_plan_builder = PythonNamespaceBuilder()
+# Initialize workflow orchestrator with namespace command builder
+namespace_orchestrator = WorkflowOrchestrator(builder=namespace_plan_builder)
+# Generate namespace commands for executor
+namespace_plans = namespace_orchestrator.generate_plans(
+    parsed_module_objects=populated_modules,
+    parsed_registry_metadata=registry_metadata,
+    workflow_config=workflow,
+)
